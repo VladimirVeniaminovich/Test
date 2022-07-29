deb677a721d6de7454be6bf9de2ce5498cea213b
Vladimir <dkflbvbh1995s@mail.ru>
2022-07-29 17:34:21 +0300

    Function for sorting strings

--- Program.cs
+++ Program.cs
@@ -4,3 +4,11 @@ string s;
 string[] ArrayString2;
 Console.WriteLine("Введите строки:");
 ArrayString = new string[count];
+void SortingStrings()
+{
+  for (int i = 0; i < ArrayString.Length; i++)
+  {
+    if (ArrayString[i].Length<=3)
+    Console.WriteLine("ArrayString[{0}] = {1}", i, ArrayString[i]);
+  }
+}
