b059122973b7dcf4273840ab3dabe37b6b94897c
Vladimir <dkflbvbh1995s@mail.ru>
2022-07-29 17:41:50 +0300

    Added block with creating array without creating Length

--- Program.cs
+++ Program.cs
@@ -12,3 +12,18 @@ void SortingStrings()
     Console.WriteLine("ArrayString[{0}] = {1}", i, ArrayString[i]);
   }
 }
+do
+{
+  s = Console.ReadLine();
+  if (s!="")
+  {
+    count++;
+    ArrayString2 = new string[count];
+    for (int i = 0; i < ArrayString2.Length - 1; i++)
+    {
+      ArrayString2[i] = ArrayString[i];
+    }
+    ArrayString2[count - 1] = s;
+    ArrayString = ArrayString2;
+  }
+} while (s != "");
